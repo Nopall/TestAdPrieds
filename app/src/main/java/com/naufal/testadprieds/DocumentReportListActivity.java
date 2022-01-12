a96948a186502b3089d55b6a16c9072f377a1dcf
Naufal Refandi <nopalakhmad@gmail.com>
2022-01-12 16:04:51 +0700

    initial commit Case Study Android Developer Prieds Representative

---
+++ app/src/main/java/com/naufal/testadprieds/DocumentReportListActivity.java
@@ -0,0 +1,14 @@
+package com.naufal.testadprieds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DocumentReportListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.document_approval_list_item_layout);
+    }
+}
